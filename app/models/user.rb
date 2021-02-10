e8e1505f613bcc952684db7ccfaee6bb0e7bc729
tenpadrummer <4460drums@gmail.com>
2021-02-10 17:17:00 +0900

    set user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ApplicationRecord
   has_secure_password
 
-  
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,100}+\z/i
+
+  with_options presence: true, uniqueness: true do |e|
+    e.validates :name
+    e.validates :email, format: { with: EMAIL_REGEX }
+  end
+
+  validates :password,  presence: true, length: { minimum: 6 }, format: { with: PASSWORD_REGEX }
 end
