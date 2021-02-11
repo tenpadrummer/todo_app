b71faeafdcfb41732b667ff3f04c54711bf1dda5
tenpadrummer <4460drums@gmail.com>
2021-02-11 12:57:19 +0900

    set task edit update function

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,16 +1,12 @@
 class TasksController < ApplicationController
   before_action :require_login, except: [:index]
+  before_action :set_task, only: [:show, :edit, :update]
 
   def index
     if logged_in?
       @tasks = Task.where(user_id: current_user.id)
     end
   end
-
-  def show
-    @task = Task.find(params[:id])
-  end
-
 
   def new
     @task = Task.new
@@ -27,10 +23,23 @@ class TasksController < ApplicationController
     end
   end
 
+  def update
+    if @task.update(task_params)
+      flash[:success] = "task successfully updated!!"
+      redirect_to task_path
+    else
+      flash[:error] = "Something went wrong"
+      render :edit
+    end
+  end
+
   private
 
   def task_params
     params.require(:task).permit(:title, :content, :deadline, :finished).merge(user_id: current_user.id)
   end
 
+  def set_task
+    @task = Task.find(params[:id])
+  end
 end
