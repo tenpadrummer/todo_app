b5102206df2e21f0d4965ed3f7b69dc6a1a78d29
tenpadrummer <4460drums@gmail.com>
2021-02-11 11:55:14 +0900

    set task create function and validations, and edit link, association

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,4 +3,25 @@ class TasksController < ApplicationController
 
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save(finished: false)
+      flash[:success] = "task successfully created!!"
+      redirect_to root_url
+    else
+      flash[:error] = "Something went wrong"
+      render :new
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :content, :deadline, :finished).merge(user_id: session[:user_id])
+  end
+
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,18 @@
 class Task < ApplicationRecord
+  belongs_to :user
+
+  with_options presence: true do |e|
+    e.validates :name
+    e.validates :content
+    e.validates :deadline
+    e.validates :user_id
+  end
+  validates_inclusion_of :finished, in: [true, false]
+  validate :correct_date?
+
+  def correct_date?
+    if self.deadline.to_time < Time.now
+      errors.add(:deadline, "Invalid.")
+    end
+  end
 end
