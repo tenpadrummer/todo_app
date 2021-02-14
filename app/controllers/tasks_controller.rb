3f2ecc18f15fb3273b21547c2955a026758d748f
tenpadrummer <4460drums@gmail.com>
2021-02-15 01:44:22 +0900

    fix some bugs

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,8 +5,8 @@ class TasksController < ApplicationController
   def index
     if logged_in?
       @tasks = Task.where(user_id: current_user.id)
-      @projects = Project.page(params[:page]).per(3)
     end
+    @projects = Project.page(params[:page]).per(3)
   end
 
   def new
